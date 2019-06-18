f76ae3c49147b022cc5af6c1452d861cd5e8323c
Devon Lumsden <devonlumsden@mail.com>
2019-06-18 14:44:37 +1200

    add menu and about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Gatsby Crash Course -Devon `,
+    description: `Running throught Gatsby crash course and excelling`,
     author: `@gatsbyjs`,
   },
   plugins: [
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const About = () => (
+    <Layout>
+        <SEO title="About" />
+        <h1>About Me</h1>
+        <p>Hey welcome to the page where I tell you about myself.</p>
+        <p>My name is Devon and this is my first time using Gatsby</p>
+    </Layout>
+)
+
+export default About
